b5a00767c35d2b0cdc0380bf087b63752b0ae674
Lexx Matula <alexamatula@gmail.com>
2024-10-07 09:53:21 -0400

    Added file

---
+++ src/main/java/com/pluralsight/Book.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class Book {
+
+    private int id;
+    private String isbn;
+    private String title;
+    private boolean iscCheckedOut;
+    private String checkoutOut;
+
+}
