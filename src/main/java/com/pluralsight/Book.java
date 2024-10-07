60f581c07a9a703473a7f5786c713400d649ca7d
Lexx Matula <alexamatula@gmail.com>
2024-10-07 10:50:35 -0400

    made book and main

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -5,7 +5,56 @@ public class Book {
     private int id;
     private String isbn;
     private String title;
-    private boolean iscCheckedOut;
-    private String checkoutOut;
+    private boolean isCheckedOut;
+    private String checkedOutTo;
 
+    public Book(int id, String title, String isbn) {
+        this.id = id;
+        this.title = title;
+        this.isbn = isbn;
+
+        this.checkedOutTo = "";
+        this.isCheckedOut = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getISBN() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+
+    public void checkOut(String name){
+        this.checkedOutTo = name;
+        this.isCheckedOut = true;
+    }
+
+    public void checkIn(){
+        this.checkedOutTo = "";
+        this.isCheckedOut = false;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", isCheckedOut=" + isCheckedOut +
+                ", checkedOutTo='" + checkedOutTo + '\'' +
+                '}';
+    }
 }
