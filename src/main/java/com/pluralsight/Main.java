81502caa24ac487d4799605bf0bb7bdcc10edfb5
Lexx Matula <alexamatula@gmail.com>
2024-10-08 09:15:31 -0400

    made a and c functional

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,5 +1,7 @@
 package com.pluralsight;
 
+import javax.smartcardio.CommandAPDU;
+
 public class Main {
 
     public static Book[] Library;
@@ -8,23 +10,86 @@ public class Main {
     public static void main(String[] args) {
         Library = GetInitializedLibrary();
 
-
         char option = PromptMainChoices();
 
-        //  System.out.println("Hello world!");
+        if(option == 'A'){
+            Book[] availableBooks = GetAvailableBooks(Library);
+                DisplayBooks(availableBooks);
 
-        //    DisplayBooks(Library);
+            //do available books display, offer to check one out.
+        } else if (option == 'C'){
+            Book[] checkedOutBooks = GetCheckedOutBooks(Library);
+                DisplayBooks(checkedOutBooks);
+            // do already check out books display
+
+        }
+
+            //this means X...
+        while (option != 'X');
+
+        //    DisplayBooks(Library)
 
     }
 
-    public static char PromptMainChoices() {
+// Go through every book
+    public static Book[] GetAvailableBooks(Book[] books){
+        Book[] checkedOut = new Book[books.length];
+        int nextIndex = 0;
+        for(Book book : books) {
+            if (!book.isCheckedOut())
+                checkedOut[nextIndex++] = book;
+
+        }
+
+        Book[] results = new Book[nextIndex];
+        System.arraycopy(checkedOut, 0, results, 0, nextIndex);
+
+        return results;
+    }
+
+    public static Book[] GetCheckedOutBooks(Book[] books){
+        Book[] checkedout = new Book[books.length];
+        int nextIndex = 0;
+        for(Book book : books){
+            if(book.isCheckedOut()){
+                checkedout[nextIndex++] = book;
+            }
+        }
+        Book[] results = new Book[nextIndex];
+        System.arraycopy(checkedout,0,results,0,nextIndex);
+
+        return results;
+    }
+
+    public static char PromptMainChoices(){
         System.out.println("Welcome to the Library!  Please select from the following choices:");
         System.out.println("    Show [A]vailable Books");
         System.out.println("    Show [C]hecked Out Books");
         System.out.println("    E[X]it the Library");
-        System.out.println("Command [A, C, X]: ");
+
+        do{
+
+            System.out.print("Command [A, C, X]: ");
+            String command = Console.PromptForString();
+
+            if ( command.equalsIgnoreCase("A")){
+                return 'A';
+            }
+            if (command.equalsIgnoreCase("C")){
+                return 'C';
+            }
+            if (command.equalsIgnoreCase("X")
+                    || command.equalsIgnoreCase("EXIT")
+                    || command.equalsIgnoreCase("Q")
+                    || command.equalsIgnoreCase("QUIT")
+            ){
+                return 'X';
+            }
+        }  while (true);
+
 
     }
+//spacing for uniform display of book info
 
     public static void DisplayBooks(Book[] books){
         System.out.printf("%5s %55s %20s %24s\n" , "ID", "TITLE", "ISBN", "CHECKOUT OUT TO" );
@@ -33,7 +98,7 @@ public class Main {
             System.out.printf("%5s %55s %20s %24s\n" , book.getId(), book.getTitle(), book.getISBN(), book.getCheckedOutTo());
         }
     }
-
+// library content
     public static Book[] GetInitializedLibrary() {
         Book[] library = new Book[20];
         library[0] = new Book(1, "Practical Tableau", "ISBN11332211");
@@ -56,7 +121,8 @@ public class Main {
         library[17] = new Book(18, "The Mythical Man-Month", "ISBN9780201835953");
         library[18] = new Book(19, "Learning JavaScript Data Structures and Algorithms", "ISBN9781785880332");
         library[19] = new Book(20, "The Elements of Programming Interviews", "ISBN9781512218237");
-        //todo: fix this data
+
+        //todo: fix this  (delete after formatting input, this is a test)
         library[3].checkOut("Matt");
         library[7].checkOut("Very Long Named John");
         return library;
